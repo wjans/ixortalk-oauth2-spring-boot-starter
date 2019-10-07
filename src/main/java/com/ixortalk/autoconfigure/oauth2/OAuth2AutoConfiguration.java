1a74656194875bc85120a7b08d79ef3abc9a4401
Ward Jans <ward.jans@wja-consulting.be>
2019-10-07 14:49:27 +0200

    Closes #11: Making it possible to disable resource server configuration based on property ixortalk.oauth2.resource-server.enabled

--- src/main/java/com/ixortalk/autoconfigure/oauth2/OAuth2AutoConfiguration.java
+++ src/main/java/com/ixortalk/autoconfigure/oauth2/OAuth2AutoConfiguration.java
@@ -150,6 +150,7 @@ public class OAuth2AutoConfiguration {
 
     @Configuration
     @Conditional(NoAuth0Condition.class)
+    @ConditionalOnProperty(prefix = "ixortalk.oauth2.resource-server", name = "enabled", havingValue = "true", matchIfMissing = true)
     @Import({ResourceServerConfiguration.class, ResourceServerTokenServicesConfiguration.class})
     protected static class PlainOAuth2Configuration {
 
